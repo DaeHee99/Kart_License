2c9a36030641c5e378218f526607d7d2a24ed73f
Daehee <mok9550@ajou.ac.kr>
2023-02-14 02:16:42 +0900

    [backend] Last Login Time Bug Fixed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -10,10 +10,12 @@ let auth = (req, res, next) => {
     User.findByToken(token, (err, user) => {
       if(err) throw err;
       if(!user) return res.status(400).json({isAuth: false, message: "인증 실패"});
-    
-      req.token = token;
-      req.user = user;
-      next();
+
+      user.update({updatedAt: new Date()}).then(result => {
+        req.token = token;
+        req.user = user;
+        next();
+      });
     })
   }
 }
