58927c4e72b1594b1ee7c9d60f2073c37496aba2
Daehee <mok9550@ajou.ac.kr>
2023-02-19 03:14:50 +0900

    [front + back] '<' Bug fixed?

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/', indexRouter);
 app.use('/api/user', userRouter);
 app.use('/api/record', recordRouter);
 app.use('/api/star', starRouter);
