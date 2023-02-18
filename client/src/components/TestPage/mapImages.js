442bcc705098fa00e67fab26aabff23d325e8adf
Daehee <mok9550@ajou.ac.kr>
2023-02-18 19:06:31 +0900

    [frontend] add new map and font

--- client/src/components/TestPage/mapImages.js
+++ client/src/components/TestPage/mapImages.js
@@ -31,6 +31,7 @@ import L3_021 from "../../images/map/021 해로비 리버스.png";
 import L3_022 from "../../images/map/022 동화마녀의성.png";
 import L3_023 from "../../images/map/023 지피의 시청광장.png";
 import L3_024 from "../../images/map/024 광산 아슬아슬 궤도전차.png";
+import L3_025 from "../../images/map/025 사막 놀라운 공룡 유적지.png";
 
 import L2_001 from "../../images/map/001 병마용.png";
 import L2_002 from "../../images/map/002 만리장성.png";
@@ -101,6 +102,7 @@ const mapImages = [
     L3_022,
     L3_023,
     L3_024,
+    L3_025,
 
     L2_001,
     L2_002,
