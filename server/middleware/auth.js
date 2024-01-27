67f633283747e20e89aa1434995d3dbaf8a24369
Daehee <mok9550@ajou.ac.kr>
2024-01-28 00:27:44 +0900

    [backend] refactor: middleware auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,23 +1,22 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
-let auth = (req, res, next) => {
-  let token = req.cookies.token;
+const auth = (req, res, next) => {
+  const token = req.cookies.token;
 
-  if(token === undefined) {
-    return res.status(200).json({isAuth: false, message: "no Token"});
-  }
-  else {
-    User.findByToken(token, (err, user) => {
-      if(err) throw err;
-      if(!user) return res.status(400).json({isAuth: false, message: "인증 실패"});
-      
-      User.updateOne({_id: user._id}, {$inc: { authCount: 1 }}).then(result => {
-        req.token = token;
-        req.user = user;
-        next();
-      });
-    })
-  }
-}
+  if (token === undefined)
+    return res.status(200).json({ isAuth: false, message: "no Token" });
+
+  User.findByToken(token, (err, user) => {
+    if (err) throw err;
+    if (!user)
+      return res.status(401).json({ isAuth: false, message: "인증 실패" });
+
+    User.updateOne({ _id: user._id }, { $inc: { authCount: 1 } }).then(() => {
+      req.token = token;
+      req.user = user;
+      next();
+    });
+  });
+};
 
 module.exports = auth;
