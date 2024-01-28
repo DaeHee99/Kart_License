b5a7c6580cc1488cb611b9219c21d5cae25f127a
Daehee <mok9550@ajou.ac.kr>
2024-01-29 01:28:19 +0900

    [frontend] feat: Delete Server Info Page

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import MyPage from "./pages/MyPage";
 import StatisticsPage from "./pages/StatisticsPage";
 import ManagerPage from "./pages/ManagerPage";
 import UserPage from "./pages/UserPage";
-import ServerPage from "./pages/ServerPage";
 
 function App() {
   const AuthLandingPage = Auth(LandingPage, null);
@@ -25,15 +24,14 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="*" element={<ServerPage />} />
-        {/* <Route path="/" element={<AuthLandingPage />} />
+        <Route path="/" element={<AuthLandingPage />} />
         <Route path="/login" element={<AuthLoginPage />} />
         <Route path="/test" element={<AuthTestPage />} />
         <Route path="/result/:id" element={<AuthResultPage />} />
         <Route path="/mypage" element={<AuthMyPage />} />
         <Route path="/statistics" element={<AuthStatisticsPage />} />
         <Route path="/manager" element={<AuthManagerPage />} />
-        <Route path="/userpage/:id" element={<AuthUserPage />} /> */}
+        <Route path="/userpage/:id" element={<AuthUserPage />} />
       </Routes>
     </div>
   );
