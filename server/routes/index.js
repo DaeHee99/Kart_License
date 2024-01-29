106d1818495eebdf6dbc3d990be6664b093fc1c9
Daehee <mok9550@ajou.ac.kr>
2024-01-30 02:56:13 +0900

    [backend] fix: Change client build directory name

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.use(express.static(path.join(__dirname, "../../client/build")));
+router.use(express.static(path.join(__dirname, "../../client/dist")));
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
 });
 
 module.exports = router;
