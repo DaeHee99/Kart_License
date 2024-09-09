b269cffbadf421345742e66ea43d721c3822cdaa
Daehee <mok9550@ajou.ac.kr>
2024-09-09 19:57:46 +0900

    [frontend] feat: New S27 track

--- client/src/global/mapImages.js
+++ client/src/global/mapImages.js
@@ -38,6 +38,7 @@ import L3_027 from "../assets/images/map/027 동화 잠자는 숲속의 거인.p
 import L3_028 from "../assets/images/map/028 WKC 상해 서킷.png";
 import L3_029 from "../assets/images/map/029 노르테유 스카이웨이.png";
 import L3_030 from "../assets/images/map/030 카멜롯 팬드래건 캐슬.png";
+import L3_031 from "../assets/images/map/031 신화 오딘의 궁전.png";
 
 import L2_001 from "../assets/images/map/001 병마용.png";
 import L2_002 from "../assets/images/map/002 만리장성.png";
@@ -123,6 +124,7 @@ const mapImages = [
   L3_028,
   L3_029,
   L3_030,
+  L3_031,
 
   L2_001,
   L2_002,
