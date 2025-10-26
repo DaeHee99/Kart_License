36f14cd64ab670d00197d2c2c87789224473fef8
Daehee <mok9550@ajou.ac.kr>
2025-10-26 15:51:36 +0900

    [frontend] feat: Season 35 Update

--- client/src/global/mapImages.js
+++ client/src/global/mapImages.js
@@ -2,35 +2,35 @@ import Rookie_001 from "../assets/images/map/001 빌리지 고가.png";
 import Rookie_002 from "../assets/images/map/002 남산.png";
 import Rookie_003 from "../assets/images/map/003 비치해변.png";
 import Rookie_004 from "../assets/images/map/004 싱가폴서킷.png";
-import Rookie_005 from "../assets/images/map/005 1920.png";
+// import Rookie_005 from "../assets/images/map/005 1920.png";
 import Rookie_006 from "../assets/images/map/006 차이나라사.png";
 import Rookie_007 from "../assets/images/map/007 용의운하.png";
 import Rookie_008 from "../assets/images/map/008 쥐공무.png";
-import Rookie_009 from "../assets/images/map/009 차이나 바다의 운율.png";
+// import Rookie_009 from "../assets/images/map/009 차이나 바다의 운율.png";
 import Rookie_010 from "../assets/images/map/010 신화 차원의 관문.png";
-import Rookie_011 from "../assets/images/map/011 더 월드.png";
-import Rookie_012 from "../assets/images/map/012 어비스 운명의 갈림길.png";
+// import Rookie_011 from "../assets/images/map/011 더 월드.png";
+// import Rookie_012 from "../assets/images/map/012 어비스 운명의 갈림길.png";
 
 import L3_001 from "../assets/images/map/001 코리아서킷.png";
 import L3_002 from "../assets/images/map/002 두바이.png";
 import L3_003 from "../assets/images/map/003 도야수.png";
-import L3_004 from "../assets/images/map/004 월드리오.png";
+// import L3_004 from "../assets/images/map/004 월드리오.png";
 import L3_005 from "../assets/images/map/005 쥐공결.png";
 import L3_006 from "../assets/images/map/006 문힐시티.png";
 import L3_007 from "../assets/images/map/007 메카닉.png";
 import L3_008 from "../assets/images/map/008 운명의다리.png";
 import L3_009 from "../assets/images/map/009 용의길.png";
-import L3_010 from "../assets/images/map/010 네모 장난감 선물 공장.png";
+// import L3_010 from "../assets/images/map/010 네모 장난감 선물 공장.png";
 import L3_011 from "../assets/images/map/011 브로디.png";
 import L3_012 from "../assets/images/map/012 노르테유전투비행장.png";
-import L3_013 from "../assets/images/map/013 고가리버스.png";
+// import L3_013 from "../assets/images/map/013 고가리버스.png";
 import L3_014 from "../assets/images/map/014 익스트림.png";
 import L3_015 from "../assets/images/map/015 월드파리.png";
 import L3_016 from "../assets/images/map/016 뉴욕.png";
 // import L3_017 from "../assets/images/map/017 이탈리아.png";
 import L3_018 from "../assets/images/map/018 차이나 서호.png";
 import L3_019 from "../assets/images/map/019 신세계.png";
-import L3_020 from "../assets/images/map/020 팩부공.png";
+// import L3_020 from "../assets/images/map/020 팩부공.png";
 import L3_021 from "../assets/images/map/021 해로비 리버스.png";
 import L3_022 from "../assets/images/map/022 동화마녀의성.png";
 import L3_023 from "../assets/images/map/023 지피의 시청광장.png";
@@ -38,14 +38,14 @@ import L3_024 from "../assets/images/map/024 광산 아슬아슬 궤도전차.pn
 import L3_025 from "../assets/images/map/025 사막 놀라운 공룡 유적지.png";
 import L3_026 from "../assets/images/map/026 네모 강철바위 용광로.png";
 import L3_027 from "../assets/images/map/027 동화 잠자는 숲속의 거인.png";
-import L3_028 from "../assets/images/map/028 WKC 상해 서킷.png";
+// import L3_028 from "../assets/images/map/028 WKC 상해 서킷.png";
 import L3_029 from "../assets/images/map/029 노르테유 스카이웨이.png";
 import L3_030 from "../assets/images/map/030 카멜롯 팬드래건 캐슬.png";
 import L3_031 from "../assets/images/map/031 신화 오딘의 궁전.png";
 import L3_032 from "../assets/images/map/032 올림포스 제우스시티.png";
-import L3_033 from "../assets/images/map/033 월드 이탈리아 여행.png";
+// import L3_033 from "../assets/images/map/033 월드 이탈리아 여행.png";
 import L3_034 from "../assets/images/map/034 카멜롯 외곽 순찰로.png";
-import L3_035 from "../assets/images/map/035 어비스 바다 소용돌이.png";
+// import L3_035 from "../assets/images/map/035 어비스 바다 소용돌이.png";
 
 import L2_001 from "../assets/images/map/001 병마용.png";
 import L2_002 from "../assets/images/map/002 만리장성.png";
@@ -69,7 +69,7 @@ import L2_019 from "../assets/images/map/019 팩두공.png";
 import L2_020 from "../assets/images/map/020 상어섬.png";
 import L2_021 from "../assets/images/map/021 포오공.png";
 import L2_022 from "../assets/images/map/022 포레스트 대관령.png";
-import L2_023 from "../assets/images/map/023 광산 3개의 지름길.png";
+// import L2_023 from "../assets/images/map/023 광산 3개의 지름길.png";
 import L2_024 from "../assets/images/map/024 쥐라기 공룡섬 대모험.png";
 import L2_025 from "../assets/images/map/025 WKC 투어링 랠리.jpg";
 import L2_026 from "../assets/images/map/026 아찔한 헬기 점프.png";
@@ -83,7 +83,7 @@ import L1_001 from "../assets/images/map/001 동이문.png";
 import L1_002 from "../assets/images/map/002 노익.png";
 import L1_003 from "../assets/images/map/003 광꼬.png";
 import L1_004 from "../assets/images/map/004 팩토리 5구역.png";
-import L1_005 from "../assets/images/map/005 포아다.png";
+// import L1_005 from "../assets/images/map/005 포아다.png";
 import L1_006 from "../assets/images/map/006 공마초.png";
 import L1_007 from "../assets/images/map/007 설산.png";
 import L1_008 from "../assets/images/map/008 리버스 직잭.png";
@@ -92,41 +92,42 @@ import L1_010 from "../assets/images/map/010 광산 위험한 제련소.png";
 import L1_011 from "../assets/images/map/011 차이나 용의 성지.png";
 import L1_012 from "../assets/images/map/012 해적 대해적 로두마니.png";
 import L1_013 from "../assets/images/map/013 아이스 부서진 빙산.png";
+import L1_014 from "../assets/images/map/014 빌리지 붐힐 드라이브.png";
 
 const mapImages = [
   Rookie_001,
   Rookie_002,
   Rookie_003,
   Rookie_004,
-  Rookie_005,
+  // Rookie_005,
   Rookie_006,
   Rookie_007,
   Rookie_008,
-  Rookie_009,
+  // Rookie_009,
   Rookie_010,
-  Rookie_011,
-  Rookie_012,
+  // Rookie_011,
+  // Rookie_012,
 
   L3_001,
   L3_002,
   L3_003,
-  L3_004,
+  // L3_004,
   L3_005,
   L3_006,
   L3_007,
   L3_008,
   L3_009,
-  L3_010,
+  // L3_010,
   L3_011,
   L3_012,
-  L3_013,
+  // L3_013,
   L3_014,
   L3_015,
   L3_016,
   // L3_017,
   L3_018,
   L3_019,
-  L3_020,
+  // L3_020,
   L3_021,
   L3_022,
   L3_023,
@@ -134,14 +135,14 @@ const mapImages = [
   L3_025,
   L3_026,
   L3_027,
-  L3_028,
+  // L3_028,
   L3_029,
   L3_030,
   L3_031,
   L3_032,
-  L3_033,
+  // L3_033,
   L3_034,
-  L3_035,
+  // L3_035,
 
   L2_001,
   L2_002,
@@ -165,7 +166,7 @@ const mapImages = [
   L2_020,
   L2_021,
   L2_022,
-  L2_023,
+  // L2_023,
   L2_024,
   L2_025,
   L2_026,
@@ -179,7 +180,7 @@ const mapImages = [
   L1_002,
   L1_003,
   L1_004,
-  L1_005,
+  // L1_005,
   L1_006,
   L1_007,
   L1_008,
@@ -188,6 +189,7 @@ const mapImages = [
   L1_011,
   L1_012,
   L1_013,
+  L1_014,
 ];
 
 export default mapImages;
